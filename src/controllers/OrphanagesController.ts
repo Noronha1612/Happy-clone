04248a90c15eef34f6f27589c23648c62e892bdb
Noronha1612 <inc.691@gmail.com>
2020-10-30 18:41:29 -0300

    Index orphanages included

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -2,5 +2,18 @@ import db from '../database/connection';
 import { Request, Response } from 'express';
 
 export default class OrphanagesController {
-    
+    async index(request: Request, response: Response) {
+        try {
+            const { orphanageId } = request.params;
+
+            const data = await db('orphanages')
+                .select('*')
+                .where(orphanageId ? { id: orphanageId } : {});
+
+            return response.status(200).json({ error: false, data });
+        } catch (err) {
+            console.log(err);
+            return response.status(500).json({ error: true, message: 'Internal Server Error' });
+        }
+    }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import orphanagesController from './controllers/OrphanagesController';
+
+const OrphanagesController = new orphanagesController();
+
 const routes = Router();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'OK' });
-});
+routes.get('/', OrphanagesController.index);
 
 export default routes;
