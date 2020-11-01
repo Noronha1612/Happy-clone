28c2f7ae807daa894abdeaacdb01d8a581e04dfb
Noronha1612 <inc.691@gmail.com>
2020-11-01 03:38:39 -0300

    Landing page included (no responsive)

--- client/src/pages/Landing/index.tsx
+++ client/src/pages/Landing/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { FaArrowRight } from 'react-icons/fa';
+
+import Illustration from '../../assets/LandingIllustration.png';
+import Logo from '../../assets/Logo.png';
 
 import './styles.css';
 
 const Landing: React.FC = () => {
   return (
-      <div className="container">
-          Landing page
+      <div className="landing-container">
+          <div className="wrapper">
+            <img src={Logo} alt="Happy"/>
+
+            <div className="content">
+                <h1>Leve felicidade para o mundo</h1>
+
+                <h3>Visite orfanatos e mude o dia de muitas crianças.</h3>
+            </div>
+
+            <img src={Illustration} alt="Faça a felicidade" className="background-illustration"/>
+
+            <div className="location">
+                <strong>Recife</strong>
+                <span>Pernambuco</span>
+            </div>
+
+            <button className="enter"><FaArrowRight/></button>
+          </div>
       </div>
   );
 }
--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import Landing from './pages/Landing';
+
 const Routes: React.FC = () => {
     return (
         <BrowserRouter>
             <Switch>
-                
+                <Route exact path="/" component={ Landing } />
             </Switch>
         </BrowserRouter>
     );
