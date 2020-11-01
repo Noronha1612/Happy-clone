d5e183a8efbdaae506f07b31fbf766e94db49ab0
Noronha1612 <inc.691@gmail.com>
2020-11-01 02:30:22 -0300

    landing page started

---
+++ client/src/pages/Landing/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+const Landing: React.FC = () => {
+  return (
+      <div className="container">
+          Landing page
+      </div>
+  );
+}
+
+export default Landing;
--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -8,7 +8,7 @@ const Routes: React.FC = () => {
                 
             </Switch>
         </BrowserRouter>
-    )
+    );
 }
 
 export default Routes;
