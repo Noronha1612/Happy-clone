edd4714943da36bf86a5628295138b4a4bff744d
Noronha1612 <inc.691@gmail.com>
2020-11-04 00:05:32 -0300

    aside bar semi-finished

--- client/src/pages/Map/index.tsx
+++ client/src/pages/Map/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
+import HappyPoint from '../../assets/HappyPoint.png';
+
 import './styles.css';
 
 const Map: React.FC = () => {
   return (
       <div className="map-container">
           <aside>
-              
+              <img src={ HappyPoint } alt="Happy" className="happy-point"/>
+
+              <main>
+                  <h1>Escolha um orfanato no mapa</h1>
+                  <h4>Muitas crianças estão <br/> esperando a sua visita :)</h4>
+              </main>
+
+              {/* Localização */}
           </aside>
       </div>
   );
