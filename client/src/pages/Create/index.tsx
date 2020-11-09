c79a50ec0247e31e123957211f67a639b6172007
NoronhaGCP <inc.691@gmail.com>
2020-11-09 02:59:25 -0300

    Add very basic validation to form

--- client/src/pages/Create/index.tsx
+++ client/src/pages/Create/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, FormEvent, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
 
@@ -7,12 +7,43 @@ import HappyPoint from '../../assets/HappyPoint.png'
 import './styles.css';
 
 const Create: React.FC = () => {
-    const [ openOnWeekends, setOpenOnWeekends ] = useState(false);
     const [ formFull, setFormFull ] = useState(true);
+
+    // Fields
+    const [ name, setName ] = useState('');
+    const [ about, setAbout ] = useState('');
+    const [ whatsapp, setWhatsapp ] = useState('');
+    const [ instructions, setInstructions ] = useState('');
+    const [ hours, setHours ] = useState('');
+    const [ openOnWeekends, setOpenOnWeekends ] = useState(false);
 
     function handleSubmit(event: FormEvent) {
         event.preventDefault();
     }
+
+    useEffect(() => {
+        function validateInputs() {
+            let allValid = true;
+    
+            if ( 
+                name.length < 3 ||
+                !about ||
+                whatsapp.length !== 11 ||
+                !instructions ||
+                !hours
+            ) allValid = false;
+    
+            return allValid;
+        }
+
+        setFormFull(validateInputs());
+    }, [
+        name,
+        about,
+        whatsapp,
+        instructions,
+        hours
+    ]);
 
     return (
         <div className="create-container" >
@@ -35,7 +66,7 @@ const Create: React.FC = () => {
 
                         <section className="input-box">
                             <label htmlFor="nameForm">Nome</label>
-                            <input type="text" id="nameForm" />
+                            <input type="text" id="nameForm" onChange={e => setName(e.target.value)} />
                         </section>
                         
                         <section className="input-box">
@@ -43,12 +74,12 @@ const Create: React.FC = () => {
                                 <label htmlFor="aboutForm">Sobre</label>
                                 <span className="aditional" >Máximo de 300 caracteres</span>
                             </span>
-                            <textarea id="aboutForm" maxLength={300} />
+                            <textarea id="aboutForm" maxLength={300} onChange={e => setAbout(e.target.value)} />
                         </section>
 
                         <section className="input-box">
                             <label htmlFor="whatsappForm">Número de Whatsapp</label>
-                            <input type="text" id="whatsappForm" />
+                            <input type="text" id="whatsappForm" onChange={e => setWhatsapp(e.target.value)} />
                         </section>
 
                         {/* <div>Fotos</div> */}
@@ -57,12 +88,12 @@ const Create: React.FC = () => {
                         
                         <section className="input-box">
                             <label htmlFor="instructionsForm">Instruções</label>
-                            <textarea id="instructionsForm" />
+                            <textarea id="instructionsForm" onChange={e => setInstructions(e.target.value)} />
                         </section>
                         
                         <section className="input-box">
                             <label htmlFor="hoursForm">Horário das visitas</label>
-                            <input type="text" id="hoursForm" />
+                            <input type="text" id="hoursForm" onChange={e => setHours(e.target.value)} />
                         </section>
 
                         <section className="bool-section">
