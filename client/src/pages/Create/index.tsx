682f2b45fa8bb5f07e00f1f59228e0ff727b6249
NoronhaGCP <inc.691@gmail.com>
2020-11-08 22:46:29 -0300

    asidebar in create page included and started field form set

--- client/src/pages/Create/index.tsx
+++ client/src/pages/Create/index.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
+import { Link } from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa';
+
+import HappyPoint from '../../assets/HappyPoint.png'
 
 import './styles.css';
 
 const Create: React.FC = () => {
-  return (
-      <div className="create-container" >
-          Create
-      </div>
-  );
+    function handleSubmit(event: FormEvent) {
+        event.preventDefault();
+    }
+
+    return (
+        <div className="create-container" >
+            <aside>
+                <img src={HappyPoint} alt="Happy" className="happy-point"/>
+
+                <Link to="/map" className="return-btn" >
+                    <FaArrowLeft size={17} /> 
+                </Link>
+            </aside>
+
+            <fieldset>
+                <form onSubmit={ handleSubmit }>
+                    <h2 className="section-title" >
+                        Dados
+                    </h2>
+
+                    {/* <div>Mapa</div> */}
+
+                    <section className="input-box">
+                        <label htmlFor="nameForm">Nome</label>
+                        <input type="text" id="nameForm" />
+                    </section>
+                </form>
+            </fieldset>
+        </div>
+    );
 }
 
 export default Create;
